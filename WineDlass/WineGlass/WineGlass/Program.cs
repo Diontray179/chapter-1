35b23b4ee6bfff31fbc7ac0dbc15b2ba20374763
Diontray179 <54554505+Diontray179@users.noreply.github.com>
2019-11-05 14:16:53 -0600

    update

---
+++ WineDlass/WineGlass/WineGlass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WineGlass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creater a wine glass graphic
+            Console.WriteLine("xxxxxxxxxxxxxxxxx");
+            Console.WriteLine("x               x");
+            Console.WriteLine(" x             x ");
+            Console.WriteLine("  xxxxxxxxxxxxx");
+            Console.WriteLine("        x      ");
+            Console.WriteLine("        x      ");
+            Console.WriteLine("     xxxxxxx    ");
+        }
+    }
+}
---
+++ personalinfo/personalinfo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personalinfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Whitneydiontray23@gmail.com);
+            Console.WriteLine("563-555-6565");
+
+        }
+    }
+}
